aaf50b27ea5178ab8f4d56227700aae63b0df41c
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2026-02-21 20:26:24 +0600

    added sass + tailwind support

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 import './index.scss'
 import App from './App.jsx'
 
